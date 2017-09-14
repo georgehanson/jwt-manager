792f39b69eaf180dbaa51cf5b91aea3f2377c598
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-09-14 15:26:08 +0100

    recompile

--- dist/build.js
+++ dist/build.js
@@ -1,1 +1,257 @@
-var asyncGenerator=function(){function e(e){this.value=e}function t(t){function n(o,i){try{var a=t[o](i),u=a.value;u instanceof e?Promise.resolve(u.value).then(function(e){n("next",e)},function(e){n("throw",e)}):r(a.done?"return":"normal",a.value)}catch(e){r("throw",e)}}function r(e,t){switch(e){case"return":o.resolve({value:t,done:!0});break;case"throw":o.reject(t);break;default:o.resolve({value:t,done:!1})}(o=o.next)?n(o.key,o.arg):i=null}var o,i;this._invoke=function(e,t){return new Promise(function(r,a){var u={key:e,arg:t,resolve:r,reject:a,next:null};i?i=i.next=u:(o=i=u,n(e,t))})},"function"!=typeof t.return&&(this.return=void 0)}return"function"==typeof Symbol&&Symbol.asyncIterator&&(t.prototype[Symbol.asyncIterator]=function(){return this}),t.prototype.next=function(e){return this._invoke("next",e)},t.prototype.throw=function(e){return this._invoke("throw",e)},t.prototype.return=function(e){return this._invoke("return",e)},{wrap:function(e){return function(){return new t(e.apply(this,arguments))}},await:function(t){return new e(t)}}}(),classCallCheck=function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")},createClass=function(){function e(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}return function(t,n,r){return n&&e(t.prototype,n),r&&e(t,r),t}}(),Local=function(){function e(){classCallCheck(this,e)}return createClass(e,[{key:"store",value:function(e){localStorage.setItem("jwt",e)}},{key:"retrieve",value:function(){var e=localStorage.getItem("jwt");return e||""}},{key:"forget",value:function(){localStorage.removeItem("jwt")}}]),e}(),Cookie=function(){function e(){classCallCheck(this,e)}return createClass(e,[{key:"store",value:function(e){var t=this.getExpiryDate();document.cookie="jwt="+e+";expires="+t.toUTCString()+";path=/"}},{key:"retrieve",value:function(){for(var e=decodeURIComponent(document.cookie).split(";"),t=e.length,n=0;n<t;n++){for(var r=e[n];" "==r.charAt(0);)r=r.substring(1);if(0==r.indexOf("jwt="))return r.substring("jwt=".length,r.length)}return""}},{key:"forget",value:function(){document.cookie="jwt=; expires=Thu, 01-Jan-70 00:00:01 GMT;"}},{key:"getExpiryDate",value:function(){var e=new Date;return new Date(e.getTime()+864e5)}}]),e}(),Token=function(){function e(t){classCallCheck(this,e),this.decoded=t,this.expiry=t.exp}return createClass(e,[{key:"getPayload",value:function(){return this.decoded}},{key:"getExpiry",value:function(){return this.expiry}}]),e}(),Decoder=function(){function e(){classCallCheck(this,e),this.store=new Cookie}return createClass(e,[{key:"useLocalStore",value:function(){this.store=new Local}},{key:"decode",value:function(){var e=this.store.retrieve();if(e){var t=e.split(".")[1].replace("-","+").replace("_","/"),n=JSON.parse(window.atob(t));return new Token(n)}throw new TypeError("No token has been set")}}]),e}(),JWTManager=function(){function e(){classCallCheck(this,e),this.secondsInterval=10,this.store=new Cookie,this.decoder=new Decoder}return createClass(e,[{key:"setToken",value:function(e){this.store.store(e)}},{key:"getToken",value:function(){return this.store.retrieve()}},{key:"forget",value:function(){this.store.forget()}},{key:"refresh",value:function(e){this.store.forget(),this.store.store(e)}},{key:"decode",value:function(){return this.decoder.decode()}},{key:"monitor",value:function(e){var t=this,n=arguments.length>1&&void 0!==arguments[1]?arguments[1]:60;setInterval(function(){try{t.decode().getExpiry()-Date.now()/1e3<=n&&e(t.getToken())}catch(e){}},this.getSecondsInterval())}},{key:"useLocalStore",value:function(){this.store=new Local,this.decoder.useLocalStore()}},{key:"getSecondsInterval",value:function(){return 1e3*this.secondsInterval}}]),e}();export default JWTManager;
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Local {
+    store(token) {
+        localStorage.setItem('jwt', token);
+    }
+    retrieve() {
+        let result = localStorage.getItem('jwt');
+        return result ? result : "";
+    }
+    forget() {
+        localStorage.removeItem('jwt');
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Local;
+
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Cookie {
+    store(token) {
+        let date = this.getExpiryDate();
+        document.cookie = `jwt=${token};expires=${date.toUTCString()};path=/`;
+    }
+    retrieve() {
+        let key = 'jwt=';
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let cookieArray = decodedCookie.split(';');
+        let cookieArrayLength = cookieArray.length;
+        for (let i = 0; i < cookieArrayLength; i++) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(key) == 0) {
+                return cookie.substring(key.length, cookie.length);
+            }
+        }
+        return "";
+    }
+    forget() {
+        document.cookie = 'jwt=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
+    }
+    getExpiryDate() {
+        let date = new Date();
+        return new Date(date.getTime() + 86400000);
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Cookie;
+
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(3);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__stores_local__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__stores_cookie__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__decoder__ = __webpack_require__(4);
+
+
+
+class JWTManager {
+    constructor() {
+        this.secondsInterval = 10;
+        this.store = new __WEBPACK_IMPORTED_MODULE_1__stores_cookie__["a" /* Cookie */]();
+        this.decoder = new __WEBPACK_IMPORTED_MODULE_2__decoder__["a" /* Decoder */]();
+    }
+    setToken(token) {
+        this.store.store(token);
+    }
+    getToken() {
+        return this.store.retrieve();
+    }
+    forget() {
+        this.store.forget();
+    }
+    refresh(token) {
+        this.store.forget();
+        this.store.store(token);
+    }
+    decode() {
+        return this.decoder.decode();
+    }
+    monitor(callback, secondsLimit = 60) {
+        setInterval(() => {
+            try {
+                let remainingSeconds = this.secondsRemaining();
+                if (remainingSeconds <= secondsLimit) {
+                    callback(this.getToken());
+                }
+            }
+            catch (e) {
+            }
+        }, this.getSecondsInterval());
+    }
+    useLocalStore() {
+        this.store = new __WEBPACK_IMPORTED_MODULE_0__stores_local__["a" /* Local */]();
+        this.decoder.useLocalStore();
+    }
+    secondsRemaining() {
+        let token = this.decode();
+        if (token) {
+            return token.getExpiry() - (Date.now() / 1000);
+        }
+    }
+    hasTokenExpired() {
+        let secondsRemaining = this.secondsRemaining();
+        if (!secondsRemaining) {
+            return false;
+        }
+        return secondsRemaining < 0;
+    }
+    getSecondsInterval() {
+        return this.secondsInterval * 1000;
+    }
+}
+/* harmony default export */ __webpack_exports__["default"] = (JWTManager);
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__stores_cookie__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__stores_local__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__token__ = __webpack_require__(5);
+
+
+
+class Decoder {
+    constructor() {
+        this.store = new __WEBPACK_IMPORTED_MODULE_0__stores_cookie__["a" /* Cookie */]();
+    }
+    useLocalStore() {
+        this.store = new __WEBPACK_IMPORTED_MODULE_1__stores_local__["a" /* Local */]();
+    }
+    decode() {
+        let token = this.store.retrieve();
+        if (token) {
+            let sectioned = token.split('.')[1];
+            let replaced = sectioned.replace('-', '+').replace('_', '/');
+            let decoded = JSON.parse(window.atob(replaced));
+            return new __WEBPACK_IMPORTED_MODULE_2__token__["a" /* Token */](decoded);
+        }
+        throw new TypeError("No token has been set");
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Decoder;
+
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Token {
+    constructor(decoded) {
+        this.decoded = decoded;
+        this.expiry = decoded.exp;
+    }
+    getPayload() {
+        return this.decoded;
+    }
+    getExpiry() {
+        return this.expiry;
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["a"] = Token;
+
+
+
+/***/ })
+/******/ ]);
